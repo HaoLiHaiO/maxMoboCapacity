4b590cbd29b9cb161c2240fbd875ca73e5ecd701
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2023-06-23 09:16:39 +0300

    fix typos

--- moboMaxCapacity.ps1
+++ moboMaxCapacity.ps1
@@ -1,10 +1,10 @@
 # This script reports the maximum RAM capacity of the system's motherboard.
 
 # Get the maximum memory capacity from WMI (in bytes)
-$maxCapacityBytes = (Get-WmiObject -Class Win32_PhysicalMemoryArray).MaxCapacity
+$maxCapacityBytes = (Get-WmiObject -Class Win32_physicalmemoryarray).maxcapacity
 
-# Convert the maximum capacity to GB and round to 2 decimal places
-$maxCapacityGB = [math]::Round($maxCapacityBytes / 1GB, 2)
+# Convert the maximum capacity to GB
+$maxCapacityGB = [math]::Round($maxCapacityBytes/1024/1024,2)
 
 # Create the output string
 $outputString = "Your motherboard supports a maximum capacity of ${maxCapacityGB} GB of RAM"
